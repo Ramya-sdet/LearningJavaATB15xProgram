365fe6cfcc09f13c48ef14cfffd0e3ca0f5f6f8b
Ramya-sdet <ramyasdet0@gmail.com>
2025-12-19 08:57:40 +0530

    Dec_19

---
+++ src/ex_03_Literals/Lab035_Non_Primitive_Literal.java
@@ -0,0 +1,15 @@
+package ex_03_Literals;
+
+public class Lab035_Non_Primitive_Literal {
+    public static void main(String[] args) {
+        String name = "Universe"; // String is bunch of char.
+        int[] arrays_of_items = new int[10];
+
+        char A = '0';
+        System.out.println(A);
+        String name1 = null;
+        String name2 = "";
+        char c = '0';
+        //int a = null;
+    }
+}
