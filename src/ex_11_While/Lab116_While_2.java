8f2aedbf1583b7b865ce95739dd0b53f1915f5c5
Ramya-sdet <ramyasdet0@gmail.com>
2026-01-07 22:18:27 +0530

    Jan_7

---
+++ src/ex_11_While/Lab116_While_2.java
@@ -0,0 +1,12 @@
+package ex_11_While;
+
+public class Lab116_While_2 {
+    public static void main(String[] args) {
+        int v = 1;
+        while (v <= 15) {
+            System.out.println("hi");
+            v++;
+        }
+    }
+}
+
