cd10fefe1a4ffb5ea77d3d53c19fae726ae26d37
Ramya-sdet <ramyasdet0@gmail.com>
2026-01-19 08:37:21 +0530

    Jan_19

---
+++ src/ex_16_Arrays/Lab151_Arrays_Part2.java
@@ -0,0 +1,18 @@
+package ex_16_Arrays;
+
+public class Lab151_Arrays_Part2 {
+    public static void main(String[] args) {
+
+        String[] name = new String[3];
+
+        name[0] = "Aditi";
+        name[1] = "Jeevitha";
+        name[2] = "Nikita";
+
+        System.out.println(name[0]);
+        System.out.println(name[2]);
+        System.out.println(name[1]);
+
+
+    }
+}
